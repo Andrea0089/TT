efcd126853f681e2556c825dbc5348809ae71e80
Andrea0089 <35372716+Andrea0089@users.noreply.github.com>
2020-01-26 14:21:46 +0100

    dssf

---
+++ app.py
@@ -0,0 +1,14 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return "<h1> Hello Worldddd </h1>"
+
+@app.route('/<name>/')
+def name(name):
+    return "<h1> Hello, <span style='color:red;'>{} </span> ! </h2>".format(name)
+
+if __name__=='__main__':
+    app.run(debug=True, host='0.0.0.0') 
